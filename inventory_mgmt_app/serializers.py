947d101767328480fad972011e8d895d3d2664fb
JTFlavier <javyflavier@gmail.com>
2023-06-06 21:12:23 +0800

    added initial support for get

--- inventory_mgmt_app/serializers.py
+++ inventory_mgmt_app/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from .models import Order, Customer, Item
 
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ["sku", "quantity"]
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ["name", "email", "address"]
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
--- inventory_mgmt_app/views.py
+++ inventory_mgmt_app/views.py
@@ -1,51 +1,77 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+# todo/todo_api/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 from .models import Order, Customer, Item
-from .serializers import OrderSerializer
+from .serializers import *
 
 # Create your views here.
 
-@csrf_exempt
-def order_list(request):
-    """
-    List all code orders, or create a new order.
-    """
-    if request.method == 'GET':
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class OrderHandlingApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = OrderSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    #def get_all_items(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+        '''
+        Get all items, regardless of availability
+        '''
+
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# def items_list(request): 
+#     """
+#     GET: return all items
+#     POST: n/a
+#     PUT: update item status (aka quantity)
+#     DELETE: delete item 
     
-@csrf_exempt
-def order_detail(request, pk):
-    """
-    Retrieve, update or delete a code order.
-    """
-    try:
-        order = Order.objects.get(pk=pk)
-    except order.DoesNotExist:
-        return HttpResponse(status=404)
+#     TODO: add behavior for available vs out of stock items
+#     """
 
-    if request.method == 'GET':
-        serializer = OrderSerializer(order)
-        return JsonResponse(serializer.data)
+# @csrf_exempt
+# def order_list(request):
+#     """
+#     List all code orders, or create a new order.
+#     """
+#     if request.method == 'GET':
+#         orders = Order.objects.all()
+#         serializer = OrderSerializer(orders, many=True)
+#         return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = OrderSerializer(order, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = OrderSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
+    
+# @csrf_exempt
+# def order_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code order.
+#     """
+#     try:
+#         order = Order.objects.get(pk=pk)
+#     except order.DoesNotExist:
+#         return HttpResponse(status=404)
 
-    elif request.method == 'DELETE':
-        order.delete()
-        return HttpResponse(status=204)
+#     if request.method == 'GET':
+#         serializer = OrderSerializer(order)
+#         return JsonResponse(serializer.data)
+
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = OrderSerializer(order, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == 'DELETE':
+#         order.delete()
+#         return HttpResponse(status=204)
+
