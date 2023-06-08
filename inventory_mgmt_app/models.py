c98d83030c54b35aaaf58cd8cd2c01f5356015e3
JTFlavier <javyflavier@gmail.com>
2023-06-08 21:10:31 +0800

    updated model for items

--- inventory_mgmt_app/models.py
+++ inventory_mgmt_app/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 class Item(models.Model):
     sku = models.CharField(max_length=8, unique=True)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length = 255, unique=True, default="")
+    price = models.DecimalField(decimal_places = 2, max_digits = 20, default=0)
+    description = models.TextField(default="")
+    quantity = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.sku)
+    
+    def save(self, *args, **kwargs):
+        self.status = self.quantity > 0
+        return super(Item, self).save(*args, **kwargs)
 
 class Customer(models.Model):
     email = models.CharField(max_length=200, unique=True)
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default="")
+    address = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return str(self.email)
